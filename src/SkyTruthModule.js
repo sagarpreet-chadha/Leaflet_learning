2f3bce35439b6467ec9528182ac2e0a430d9d0a4
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2018-10-23 23:40:33 +0530

    Update SkyTruthModule.js

--- src/SkyTruthModule.js
+++ src/SkyTruthModule.js
@@ -20,7 +20,7 @@ module.exports = function SkyTruthModule(op) {
 
     function onMapLoad(e){} */
     var greenIcon = L.icon({
-      iconUrl: 'http://www.clker.com/cliparts/T/G/b/7/r/A/red-dot.svg',
+      iconUrl: 'https://www.clker.com/cliparts/T/G/b/7/r/A/red-dot.svg',
       iconSize:     [30, 20], // size of the icon
       iconAnchor:   [20 , 0], // point of the icon which will correspond to marker's location
       popupAnchor:  [-5, -5] // point from which the popup should open relative to the iconAnchor
